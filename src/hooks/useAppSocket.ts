a836e8da5ed7edcf788650ea258b74e649127ef8
vishalcs106 <vishalcs106@rocketmail.com>
2023-11-09 10:37:31 +0530

    Added rainbowkit

--- src/hooks/useAppSocket.ts
+++ src/hooks/useAppSocket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", { transports: ["websocket"] });
+const socket = io("http://localhost:5000", { transports: ["websocket"] });
 const useAppSocket = () => {
   const joinGame = (playerName: string) => {
     return socket.emit("joinGame", playerName);
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, polygon],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "RainbowKit demo",
+  projectId: "fbe96b8c13b22e85b8136876e27abd48",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+console.log("wagmoconf " + JSON.stringify(wagmiConfig));
+console.log("chains " + JSON.stringify(chains));
+
+root.render(
+  // <React.StrictMode>
+  //   <WagmiConfig config={wagmiConfig}>
+  //     <RainbowKitProvider chains={chains}>
+  //       <App />
+  //     </RainbowKitProvider>
+  //   </WagmiConfig>
+  // </React.StrictMode>
+  <WagmiConfig config={wagmiConfig}>
+    <RainbowKitProvider chains={chains}>
+      <App />
+    </RainbowKitProvider>
+  </WagmiConfig>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
